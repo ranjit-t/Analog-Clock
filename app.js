a10bb807e44b436013c52fa24e985f5693589352
Ranjith Thota <amailtoranjith@gmail.com>
2023-01-31 13:27:48 +0100

    second commit

--- app.js
+++ app.js
@@ -1,66 +1,60 @@
-let hoursArray =["12","01","02","03","04","05","06","07","08","09","10","11","12","01","02","03","04","05","06","07","08","09","10","11"];
+let hoursArray = [
+  "12",
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+];
 
+let handSecs = document.querySelector(".sec");
+let handMins = document.querySelector(".min");
+let handHrs = document.querySelector(".hrs");
 
-
-let digHrs = document.querySelector('.digHrs');
-let digMins = document.querySelector('.digMins');
-let digSecs = document.querySelector('.digSecs');
-
-let handSecs = document.querySelector('.sec');
-let handMins = document.querySelector('.min');
-let handHrs = document.querySelector('.hrs');
-
-
-function showTime() {
+function secsMovement() {
   let date = new Date();
-let secs = date.getSeconds();
-let mins = date.getMinutes();
-let hours = date.getHours();
-  
-  digSecs.innerHTML = secs;
-  digMins.innerHTML = mins;
-  digHrs.innerHTML = hours;
-  setTimeout(showTime, 1000);
+  const secs = date.getSeconds();
+  let secDegrees = secs * 6;
+  handSecs.style.transform = `rotate(${secDegrees}deg)`;
+
+  setTimeout(secsMovement, 1000);
 }
 
-showTime();
-
-
-
-
-
-
-
-
-
-
-function secsMovement(){
-let date = new Date();
-const secs = date.getSeconds();
-  let secDegrees = secs*6;
- handSecs.style.transform =`rotate(${secDegrees}deg)`;
-
- setTimeout(secsMovement, 1000);
-}
-
-function minsMovement(){
+function minsMovement() {
   let date = new Date();
   const mins = date.getMinutes();
-  let minDegrees = mins*6;
-  handMins.style.transform =`rotate(${minDegrees}deg)`;
+  let minDegrees = mins * 6;
+  handMins.style.transform = `rotate(${minDegrees}deg)`;
 
-   setTimeout(minsMovement, 1000);
+  setTimeout(minsMovement, 1000);
 }
 
-function hoursMovement(){
-   let date = new Date();
-const hours = date.getHours();
+function hoursMovement() {
+  let date = new Date();
+  const hours = date.getHours();
   const mins = date.getMinutes();
-  let minDegrees = mins*6;
-let hourDegrees = hoursArray[hours]*30 +mins*0.5;
- handHrs.style.transform =`rotate(${hourDegrees}deg)`;
-  
-  
+  let minDegrees = mins * 6;
+  let hourDegrees = hoursArray[hours] * 30 + mins * 0.5;
+  handHrs.style.transform = `rotate(${hourDegrees}deg)`;
+
   setTimeout(hoursMovement, 1000);
 }
 
